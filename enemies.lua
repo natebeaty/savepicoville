0ec6a4f5876f933ba7717e6b195be93445f623bb
Nate Beaty <nate@clixel.com>
2023-10-18 17:47:08 -0400

    add enemy dying state with animation

--- enemies.lua
+++ enemies.lua
@@ -7,28 +7,37 @@ end
 
 -- construct new enemy
 new_enemy = function(x, y)
-  local obj = {x=x, y=y, dx=0, dy=0.5, sprite=16, t=0}
+  local obj = {x=x, y=y, dx=0, dy=0.5, sprite=16, t=0, dying=0}
   obj.box = {x1=0,y1=3,x2=7,y2=7}
 
   obj.update = function(this)
-    this.t+=1
-    if (this.t>5) then
-      this.sprite+=1
-      this.t=0
+    -- hitting player?
+    if (this.dying==0 and p.dying==0 and coll(this,p)) then
+      p.die()
+      this.die(this)
     end
-    if (this.sprite)>17 this.sprite=16
 
-    if (rnd(100) > 97) this.dx = (rnd(5) - 2.5)/2
-    if (rnd(100) > 97) this.dy = (rnd(3) - 1.5)/2
-    --move the enemy
-    this.x += this.dx
-    this.y += this.dy
-    if (this.y < -10) then
-      this.dy = 0.5
-      this.y = -10
+    if (this.dying>0) then
+      this.dying-=1
+      if t%3==0 then this.sprite=32 else this.sprite=33 end
+      if this.dying==0 then
+        del(enemies, this)
+      end
+    else
+      if t%5==0 then this.sprite=16 else this.sprite=17 end
+
+      if (rnd(100) > 97) this.dx = (rnd(5) - 2.5)/2
+      if (rnd(100) > 97) this.dy = (rnd(3) - 1.5)/2
+      --move the enemy
+      this.x += this.dx
+      this.y += this.dy
+      if (this.y < -10) then
+        this.dy = 0.5
+        this.y = -10
+      end
+      -- offscreen?
+      if (this.x < -10 or this.x > 138) del(enemies, this)
     end
-    -- offscreen?
-    if (this.x < -10 or this.x > 138) del(enemies, this)
   end
 
   obj.draw = function(this)
@@ -37,7 +46,11 @@ new_enemy = function(x, y)
 
   --bye bye
   obj.die = function(this)
-    del(enemies, this)
+    sfx(02)
+    sfx(03)
+    this.dx = 0
+    this.dy = 0
+    this.dying=10
   end
 
   --return the enemy
