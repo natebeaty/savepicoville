c694e9982a62ab96e0729b32ad9133ff27443ef0
Nate Beaty <nate@clixel.com>
2023-10-18 23:09:42 -0400

    add mode handling for title, game, and game over

--- main.lua
+++ main.lua
@@ -1,6 +1,15 @@
 --save new york!
 
+function game_over()
+  t=0
+  enemies={}
+  p.respawn()
+  sfx(04)
+  mode="game over"
+end
+
 function _init()
+  mode="title"
   bullets={}
   enemies={}
   gameover=false
@@ -9,56 +18,100 @@ function _init()
   t=0
   transition_speed=0.25
   make_player()
+  music(00)
 end
 
 function _update()
   t+=1
-  if (t%15==0) then
-    if (p.dy!=0 or p.dx!=0) p.fuel-=1
-    t=0
+
+  if mode=="title" then
+
+    check_enemy_spawn(5)
+    foreach(enemies, function(obj)
+      obj.update(obj)
+    end)
+    if (t>5 and btn(4)) then
+      sfx(05)
+      enemies={}
+      mode="game"
+    end
+
+  elseif mode=="game" then
+
+    if (t%15==0) then
+      -- moving?
+      if (p.dy!=0 or p.dx!=0) p.fuel-=1
+    end
+
+    move_player()
+    player_fire()
+    check_enemy_spawn(2)
+    --update_map()
+    foreach(bullets, function(obj)
+      obj.update(obj)
+    end)
+    foreach(enemies, function(obj)
+      obj.update(obj)
+    end)
+
+  elseif mode=="game over" then
+
+    if (t>5 and btn(4)) then
+      t=0
+      p.score=0
+      p.life="♥♥♥"
+      mode="title"
+    end
+
   end
-  move_player()
-  player_fire()
-  check_enemy_spawn()
-  if (count(enemies)>2 and rnd(100) > 95) add(enemies, new_enemy(rnd(128), -10))
-  -- update_map()
-  foreach(bullets, function(obj)
-    obj.update(obj)
-  end)
-  foreach(enemies, function(obj)
-    obj.update(obj)
-  end)
 end
 
-function game_over()
-  gameover=true
-end
-
+-- draw!
 function _draw()
-  if gameover then return end
   cls()
 
-  -- update_camera()
+  if mode=="title" then
 
-  --draw the map from tile 0,0
-  --at screen coordinate 0,0 and
-  --draw 16 tiles wide and tall
-  map(0,0,0,0,128,32)
+    map(0,0,0,0,128,32)
+    foreach(enemies, function(obj)
+      obj.draw(obj)
+    end)
+    rectfill(0,0,128,8,14)
+    print("beaty softworks presents",17,2,1)
+    print("save chicago!",40,24,1)
+    print("save chicago!",40,23,7)
+    print("🅾️ start game",40,43,7)
 
-  --draw the player's sprite at
-  --p.x,p.y
-  spr(p.sprite, p.x, p.y, 1, 1, p.flipx, p.flipy)
+  elseif mode=="game" then
 
-  -- update_map()
-  foreach(bullets, function(obj)
-    obj.draw(obj)
-  end)
-  foreach(enemies, function(obj)
-    obj.draw(obj)
-  end)
+    --update_camera()
+    map(0,0,0,0,128,32)
 
-  rectfill(0,0,128,8,14)
-  print("fuel:"..p.fuel, 4, 2, 1)
-  print("score:"..p.score, 50, 2, 1)
-  print(p.life, 100, 2, 1)
+    --draw player
+    spr(p.sprite, p.x, p.y, 1, 1, p.flipx, p.flipy)
+
+    --update_map()
+    foreach(bullets, function(obj)
+      obj.draw(obj)
+    end)
+    foreach(enemies, function(obj)
+      obj.draw(obj)
+    end)
+
+    --status bar
+    rectfill(0,0,128,8,14)
+    print("fuel:"..p.fuel, 4, 2, 1)
+    print("score:"..p.score, 50, 2, 1)
+    print(p.life, 100, 2, 1)
+
+  elseif mode=="game over" then
+
+      map(0,0,0,0,128,32)
+      rectfill(0,0,128,8,14)
+      print("score:"..p.score, 50, 2, 1)
+      -- print("beaty softworks presents",17,2,1)
+      print("game  over",44,24,1)
+      print("🅾️ restart",44,43,7)
+
+  end
 end
