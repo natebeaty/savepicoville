3ba294c11eecf91e3264ba4b4322a87462ed1cdf
Nate Beaty <nate@clixel.com>
2023-10-23 22:32:45 -0400

    end game if all buildings destroyed

--- buildings.lua
+++ buildings.lua
@@ -1,6 +1,6 @@
 -- buildings
 
-function building_update(chk)
+function building_blink(chk)
   if t%chk==0 then
     -- blink undamaged building lights
     for i=1,#buildings do
@@ -10,7 +10,7 @@ function building_update(chk)
           local map_y=14-buildings[i].height+h
           local map_sprite=mget(map_x,map_y)
           if (chk==1 or rnd()>0.98) and fget(map_sprite,1) and not fget(map_sprite,2) then
-            mset(map_x, map_y, flr(rnd(5))+1)
+            mset(map_x,map_y,flr(rnd(5))+1)
           end
         end
       end
@@ -20,17 +20,21 @@ end
 
 -- create randomized buildings
 function make_buildings()
-  -- reset to blue
+  rumblingrows={}
+  -- reset all building tiles to sky
   for x=0,16 do
     for y=0,14 do
       mset(x,y,8)
     end
   end
 
+  -- reset peopleleft
+  peopleleft=0
+
   -- generate random buildings
   buildings={}
   lastx=1
-  for i=0,3 do
+  for i=1,4 do
     building={
       height=flr(rnd(5)+3),
       width=flr(rnd(2))+2,
@@ -39,10 +43,11 @@ function make_buildings()
     lastx=lastx+building.width+(flr(rnd(3)))
     for n=1,building.height do
       for x=1,building.width do
-        mset(building.x+x, 15-n, flr(rnd(5))+1)
+        mset(building.x+x,15-n,flr(rnd(5))+1)
       end
     end
-    add(buildings, building)
+    peopleleft+=building.height*building.width
+    add(buildings,building)
   end
 end
 
@@ -53,13 +58,13 @@ function check_building_hit(obj,grp)
   local ny_t=obj.y+obj.dy+obj.box.y1
   local ny_b=obj.y+obj.dy+obj.box.y2
   -- set variable so we can use "or"
-  local foo = is_undamaged_building(nx_l,ny_t,obj,grp) or
-    is_undamaged_building(nx_l,ny_b,obj,grp) or
-    is_undamaged_building(nx_r,ny_t,obj,grp) or
-    is_undamaged_building(nx_r,ny_b,obj,grp)
+  local foo = is_undamaged_brick(nx_l,ny_t,obj,grp) or
+    is_undamaged_brick(nx_l,ny_b,obj,grp) or
+    is_undamaged_brick(nx_r,ny_t,obj,grp) or
+    is_undamaged_brick(nx_r,ny_b,obj,grp)
 end
 
-function is_undamaged_building(x,y,obj,grp)
+function is_undamaged_brick(x,y,obj,grp)
   local map_x=flr(x/8)
   local map_y=flr(y/8)
   local map_sprite=mget(map_x,map_y)
@@ -69,7 +74,7 @@ function is_undamaged_building(x,y,obj,grp)
     else del(grp,obj) end
     sfx(1)
     new_explosion(map_x*8,map_y*8)
-    -- set random building damaged sprite
+    -- random damaged sprite
     mset(map_x,map_y,6+flr(rnd(2)))
     -- check building rows for collapse
     check_building_collapse()
@@ -91,17 +96,19 @@ function check_building_collapse()
         end
       end
       if rowbusted then
-        add(buildingcrash, {building=building,rowbusted=i})
+        -- collapsing rubble
+        add(buildingcrash,{building=building,rowbusted=i})
       end
     end
   end
 end
 
+-- row to collapse
 function new_rumblingrow(x,y,delay)
   local obj={x=x,y=y,delay=delay,t=0}
   obj.update=function(this)
     this.t+=1
-    if (this.t > this.delay+15) del(rumblingrows, this)
+    if (this.t > this.delay+15) del(rumblingrows,this)
   end
   obj.draw=function(this)
     local sprite=24
@@ -115,4 +122,28 @@ function new_rumblingrow(x,y,delay)
     spr(sprite,this.x,this.y)
   end
   return obj
+end
+
+-- building update loop
+function building_update()
+  if #buildingcrash>0 then
+    for obj in all(buildingcrash) do
+      for i=1,obj.rowbusted do
+        sfx(11)
+        for j=1,obj.building.width do
+          local mx=obj.building.x+j
+          local my=14-obj.building.height+i
+          mset(mx,my,8)
+          add(rumblingrows,new_rumblingrow(mx*8,my*8,i*10))
+        end
+        -- any undamaged brick left?
+        peopleleft-=obj.building.width
+      end
+      -- shorten building height
+      obj.building.height-=obj.rowbusted
+    end
+    -- you killed chicago!
+    if (peopleleft<=0) game_over()
+    buildingcrash={}
+  end
 end
