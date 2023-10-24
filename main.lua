223fd787ca494d779fab85fc57167c9b249c1bc0
Nate Beaty <nate@clixel.com>
2023-10-23 22:38:47 -0400

    add level support with enemies_killed tracking, bonus mode with end-of-level extra points, update colors, hi-score support

--- main.lua
+++ main.lua
@@ -8,31 +8,64 @@ function minspeed(spd,minspd)
   return spd
 end
 
+function pad(string,length)
+  string=""..string
+  if (#string==length) return string
+  return "0"..pad(string,length-1)
+end
+
 function _init()
+  t=0
+  level=1
+  hiscore=0
+  enemies_killed=0
   mode="title"
   explosions={}
   buildingcrash={}
   rumblingrows={}
+  peopleleft=100
   bullets={}
   enemies={}
   supply={}
   balloon={}
-  gameover=false
-  mx,my=0,0 --movement
-  cx,cy=0,0 --camera
-  t=0
-  transition_speed=0.25
+  bonuscheck={x=0,y=15}
+  bonuslastbrick=nil
   make_player()
   make_buildings()
-  music(00, 2500)
+  music(0,2500)
 end
 
 function game_over()
   t=0
-  enemies={}
+  hiscore=max(hiscore,p.score)
+  level=1
   p.respawn()
   sfx(04)
   mode="game over"
+end
+
+function status_bar()
+  rectfill(0,0,128,8,1)
+  print("fuel:"..flr(p.fuel),4,2,9)
+  print("score:"..p.score.."0",48,2,9)
+  print(p.life,100,2,9)
+end
+
+function check_level()
+  if (enemies_killed>=5 and level==1 or enemies_killed>=10) then
+    enemies_killed=0
+    enemies={}
+    supply={}
+    balloon={}
+    bullets={}
+    t=0
+    mode="bonus"
+    -- set min/max starting points for bonuscheck
+    for building in all(buildings) do
+      bonuscheck.y = min(bonuscheck.y,15-building.height)
+      bonuscheck.x = max(bonuscheck.x,building.x+building.width)
+    end
+  end
 end
 
 function start_game()
@@ -48,51 +81,79 @@ function restart()
   p.life="♥♥♥"
   mode="title"
   make_buildings()
-  music(00, 2500)
+  music(0,2500)
 end
-
 
 function _update()
   t+=1
-
-  if #buildingcrash>0 then
-    for obj in all(buildingcrash) do
-      for i=1,obj.rowbusted do
-        sfx(11)
-        for j=1,obj.building.width do
-          local mx=obj.building.x+j
-          local my=14-obj.building.height+i
-          mset(mx, my, 8)
-          add(rumblingrows, new_rumblingrow(mx*8,my*8,i*10))
-        end
-      end
-    end
-    buildingcrash={}
-  end
-  for obj in all(enemies) do
-    obj.update(obj)
-  end
-
+  building_update()
 
   if mode=="title" then
 
-    building_update(4)
+    building_blink(4)
     check_enemy_spawn(5)
-    for obj in all(enemies) do
-      obj.update(obj)
+    for grp in all({enemies,supply,balloon}) do
+      for obj in all(grp) do
+        obj.update(obj)
+      end
     end
     -- enough time has elapsed and btn is pressed
     if t>5 and btn(4) then
       start_game()
     end
 
+  elseif mode=="bonus" then
+
+    for grp in all({explosions,rumblingrows}) do
+      for obj in all(grp) do
+        obj.update(obj)
+      end
+    end
+    --
+    if bonuslastbrick~=nil then
+      mset(bonuslastbrick.x,bonuslastbrick.y,05)
+      bonuslastbrick=nil
+    end
+    -- tally bonus points
+    if (bonuscheck.y<16) then
+      local map_sprite=mget(bonuscheck.x,bonuscheck.y)
+      -- skip sky until brick
+      while map_sprite==9 and bonuscheck.y<16 do
+        bonuscheck.x-=1
+        if bonuscheck.x<0 then
+          bonuscheck.y+=1
+          bonuscheck.x=15
+        end
+      end
+      -- unbroken brick
+      if fget(map_sprite,1) and not fget(map_sprite,2) then
+        p.score+=1
+        bonuslastbrick={x=bonuscheck.x,y=bonuscheck.y}
+        sfx(12)
+        mset(bonuscheck.x,bonuscheck.y,map_sprite+48) --white flash
+      end
+      bonuscheck.x-=1
+      if bonuscheck.x<0 then
+        bonuscheck.y+=1
+        bonuscheck.x=15
+      end
+    else
+      building_blink(1)
+      if (t>5 and btn(4)) then
+        -- add remaining bonus
+        t=0
+        level+=1
+        p.respawn()
+        mode="game"
+      end
+    end
+
   elseif mode=="game" then
 
     p.update()
-    building_update(3)
-    check_enemy_spawn(2)
+    building_blink(3)
+    check_enemy_spawn(level)
     check_supply_spawn()
-    update_map()
     for grp in all({bullets,enemies,supply,balloon,explosions,rumblingrows}) do
       for obj in all(grp) do
         obj.update(obj)
@@ -101,8 +162,8 @@ function _update()
 
   elseif mode=="game over" then
 
-    building_update(1)
-    for grp in all({supply,balloon,explosions,rumblingrows}) do
+    building_blink(1)
+    for grp in all({enemies,supply,balloon,explosions,rumblingrows}) do
       for obj in all(grp) do
         obj.update(obj)
       end
@@ -125,45 +186,54 @@ function _draw()
 
   if mode=="title" then
 
-    for obj in all(enemies) do
-      obj.draw(obj)
+    for grp in all({enemies}) do
+      for obj in all(grp) do
+        obj.draw(obj)
+      end
     end
-    rectfill(0,0,128,8,14)
+    rectfill(0,0,128,8,1)
+    rectfill(16,1,112,7,9)
     print("beaty softworks presents",17,2,1)
-    print("save chicago!",40,24,1)
-    print("save chicago!",40,23,7)
-    print("🅾️ start game",40,43,7)
+    print("save chicago!",39,31,1)
+    print("save chicago!",39,30,9)
+    print("save chicago!",39,29,7)
+    print("🅾️ start game",39,45,1)
+    rectfill(0,119,128,128,1)
+    print("hi-score:"..pad(hiscore.."0",6),34,121,9)
 
   elseif mode=="game" then
 
-    --update_camera()
-
-    update_map()
     for grp in all({bullets,enemies,supply,balloon,explosions,rumblingrows}) do
       for obj in all(grp) do
         obj.draw(obj)
       end
     end
     p.draw()
+    status_bar()
 
-    --status bar
-    rectfill(0,0,128,8,14)
-    print("fuel:"..flr(p.fuel), 4, 2, 1)
-    print("score:"..p.score, 50, 2, 1)
-    print(p.life, 100, 2, 1)
+  elseif mode=="bonus" then
+
+    for grp in all({explosions,rumblingrows}) do
+      for obj in all(grp) do
+        obj.draw(obj)
+      end
+    end
+    status_bar()
+    print("level "..level.." complete",32,31,1)
+    if bonuscheck.y<16 and t%30<15 then
+      print("bonus points",40,45,1)
+    end
 
   elseif mode=="game over" then
 
-      rectfill(0,0,128,8,14)
-      print("score:"..p.score, 50, 2, 1)
-      -- print("beaty softworks presents",17,2,1)
-      print("game  over",44,24,1)
-      print("🅾️ restart",44,43,7)
-
-      for grp in all({supply,balloon,explosions}) do
-        for obj in all(grp) do
-          obj.draw(obj)
-        end
+    for grp in all({enemies,supply,balloon,explosions}) do
+      for obj in all(grp) do
+        obj.draw(obj)
       end
+    end
+      rectfill(0,0,128,8,1)
+      print("score:"..p.score.."0",48,2,9)
+      print("game  over",44,31,1)
+      print("🅾️ restart",44,45,1)
   end
 end
